55bb6bb7eb78a9da8ae10d94dbec037380327f44
Disha Chaudhary <dishachaudhary129@gmail.com>
2026-02-16 10:37:48 +0530

    postman

--- server.js
+++ server.js
@@ -5,3 +5,20 @@ const app = express();
 
 app.use(express.static(path.join(__dirname,'public')));
 
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(publicPath,"signup.html"));
+})
+
+//submit from GET
+//postman
+//vercel or render ke thorugh project ko live 
+//mongoDB-compass
+a
+
+app.get("/submit",(req,res)=>{
+    const {Firstname,Lastname,gender}= req.query});
+
+    res.send(
+        `<h2>Form Submitted Successfully!!</h2>
+        <p>Firstname : ${Firstname}</p>`
+    )
