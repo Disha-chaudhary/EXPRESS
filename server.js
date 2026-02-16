7a051acaee18c7e38e75f9c7c57beba3ad372801
Disha Chaudhary <dishachaudhary129@gmail.com>
2026-02-16 11:02:57 +0530

    form

--- server.js
+++ server.js
@@ -1,24 +1,36 @@
 const express = require("express");
 const path = require("path");
+const fs = require('fs');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname,'public')));
 
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(publicPath,"signup.html"));
-})
+
 
 //submit from GET
 //postman
 //vercel or render ke thorugh project ko live 
 //mongoDB-compass
-a
 
-app.get("/submit",(req,res)=>{
-    const {Firstname,Lastname,gender}= req.query});
 
-    res.send(
-        `<h2>Form Submitted Successfully!!</h2>
-        <p>Firstname : ${Firstname}</p>`
-    )
+app.post("/submit",(req,res)=>{
+    const username = req.body.username;
+    const email = req.body.email;
+    const gender = req.body.gender;
+
+    const data = `name:${name},email:${email}\n`;
+    
+    fs.appendFile("data.txt",data,(err)=>{
+        if(err){
+            console.log("Error saving data");
+            res.send("Error saving data");
+        }
+        else{
+            console.log("Data saved Successfullyy!!!");
+        }
+    });
+});
+app.listen(5600,()=>{
+    ("server running on port 5400");
+})
